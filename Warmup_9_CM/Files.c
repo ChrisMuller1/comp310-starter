5e40f12adc9981c81b213cd3818dacab2f690fa5
Chris Muller <chrismuller435@gmail.com>
2024-01-29 04:33:00 +0000

    Forgot to close the file once read

--- Warmup_9_CM/Files.c
+++ Warmup_9_CM/Files.c
@@ -2,7 +2,8 @@
 int main() {
     const char* filename = "x.txt";
     FILE* fileWrite = fopen(filename, "w");
-    fprintf(fileWrite, "Hello, World!");
+    fprintf(fileWrite, "This is my file");
     fclose(fileWrite);
     FILE* fileRead = fopen(filename, "r");
+    fclose(fileRead);
 }
